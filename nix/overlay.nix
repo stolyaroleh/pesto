7bea013fe67e8596db476904358a44d36a1b0410
Oleh Stolyar <stolyar.oleh@gmail.com>
2021-01-20 12:56:48 +0000

    Add bazel 4.0.0rc10
    From https://github.com/NixOS/nixpkgs/pull/106984

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,5 +1,11 @@
+let
+  sources = import ./sources.nix;
+  bazel4nixpkgs = import sources.bazel4nixpkgs { };
+  bazel = bazel4nixpkgs.bazel_4;
+in
 self: super:
 {
+  inherit bazel;
   nix2bazel = rec {
     tools = {
       bazel-compdb = super.callPackage ../compilation_database { };
